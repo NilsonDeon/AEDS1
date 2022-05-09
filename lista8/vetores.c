21bb71fe25baece27e2b330df39beafe0d88e849
Nilson Deon <NilsonDeon01@gmail.com>
2022-05-09 16:44:52 -0300

    finished

--- lista8/vetores.c
+++ lista8/vetores.c
@@ -6,12 +6,13 @@
 
 int* aloca(int n, int preenche){
     int *p = NULL;
+
     if(preenche == TRUE){
-        p = (int*) calloc(n , (sizeof(int)));
+        p = (int*) calloc(n , (sizeof(int))); // Criando vetor sem atribuir conteúdo
     }
     else if (preenche == FALSE)
     {
-        p = (int*) malloc(n * (sizeof(int)));
+        p = (int*) malloc(n * (sizeof(int))); // Criando vetor com conteúdo 0
     }
     
     return p;
@@ -20,7 +21,7 @@ int* aloca(int n, int preenche){
 void imprime(int *v, int n){
     for (int i = 0; i < n; i++){
     
-        printf("%i ", *(v+i));
+        printf("%i ", *(v+i)); // Imprimindo conteúdo na posição i
     }
     
 }
@@ -28,17 +29,16 @@ void imprime(int *v, int n){
 int* Preenche(int *v, int n, int valor, int is_aleatorio){
     if (is_aleatorio == TRUE)
     {
-        for (int i = 0; i < n; i++)
-        {
-            int num = rand()%100;
-            *(v+i) += num;
+        for (int i = 0; i < n; i++){
+            int num = rand()%100; 
+            *(v+i) = num; // Atribuindo número aleatório para a posição i
         }
       
     }
-    else{
+    else if (is_aleatorio == FALSE){
         for (int i = 0; i < n; i++)
         {
-           *(v+i) += valor;
+           *(v+i) = valor; // Atribuindo valor para a posição i
         }
         
     }
@@ -52,26 +52,52 @@ void inverte(int *v, int n){
 
     for (int i = 0; i < n; i++)
     {
-        vet[i] = *(v+i);
+        vet[i] = *(v+i); // Copiando o vetor recebido para outro 
     }
 
-    for (int i = n-1; i > 0; i--)
+    for (int i = n-1; i >= 0; i--)
     {
-       *(v+cont) = vet[i];
+       *(v+cont) = vet[i]; // Invertendo vetor
        cont++;
     }
     
 }
 
 int main(){
-    int tamanho = 5, *r = NULL, valor = 0;
-    
-    r = aloca(tamanho, FALSE);
-    r = Preenche(r, tamanho, valor, TRUE);
-    imprime(r, tamanho);
-    inverte(r, tamanho);
+
+    // f)
+    int *v1, *v2; 
+
+    // g)
+    v1 = aloca(5, FALSE); 
+
+    // h)
+    v2 = aloca(2, TRUE); 
+
+    // i)
+    imprime(v1, 5);
     printf("\n");
-    imprime(r, tamanho);
+    imprime(v2, 2);
+    printf("\n");
+
+    // j)
+    printf("\n");
+    v1 = Preenche(v1, 5, 0, TRUE);
+
+    // k)
+    imprime(v1, 5);
+    printf("\n");
+
+    // l)
+    printf("\n");
+    inverte(v1, 5);
+    inverte(v2, 2);
+
+    // m)
+    imprime(v1, 5);
+    printf("\n");
+    imprime(v2, 2);
+    printf("\n");
 
     return 0;
 }
